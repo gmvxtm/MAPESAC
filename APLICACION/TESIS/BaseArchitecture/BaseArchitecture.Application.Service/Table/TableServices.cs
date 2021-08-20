28ecc8c81acfcdc28e10c0178441dcf38a903c17
Gino Angelo Maguina Vera <gino.maguina@mdp.com.pe>
2021-08-20 12:14:45 -0500

    cambios

--- APLICACION/TESIS/BaseArchitecture/BaseArchitecture.Application.Service/Table/TableServices.cs
+++ APLICACION/TESIS/BaseArchitecture/BaseArchitecture.Application.Service/Table/TableServices.cs
@@ -15,109 +15,16 @@ using System.Transactions;
 using System.Web.Http;
 using map = AutoMapper;
 using Util = BaseArchitecture.Cross.SystemVariable.Util;
+using BaseArchitecture.Repository.Entity;
 
 namespace BaseArchitecture.Application.Service.Table
 {
-    public class TableServices : IDemoService
+    public class TableServices : ITableService
     {
-        public IDemoQuery DemoQuery { get; set; }
-        public IDemoTransaction DemoTransaction { get; set; }
-        public Trace TraceLogger =>
-            (Trace)GlobalConfiguration.Configuration.DependencyResolver.GetService(typeof(Trace));
-        public Response<CollectionDataResponse<PersonResponse>> ListPersonAll(PersonFilterRequest personFilterRequest)
-        {
-            var result = DemoQuery.ListPersonAll(personFilterRequest).ToList();
-            var typeOrder = (personFilterRequest.Pagination.TypeOrder == "desc")
-                ? Util.Extension.TypeOrder.Desc
-                : Util.Extension.TypeOrder.Asc;
-            var pros = Util.Page<PersonResponse>.CreateInstance(personFilterRequest.Pagination.CurrentPage,
-                personFilterRequest.Pagination.RowsPerPage, result);
-            var listResult = pros.Filter(personFilterRequest.FilterColumn)
-                .Order(personFilterRequest.Pagination.ColumnOrder, typeOrder)
-                .Pagination().Collection;
-            var collectionDataResponse = new CollectionDataResponse<PersonResponse>()
-            {
-                Collection = listResult,
-                Pagination = new PaginationResponse()
-                {
-                    QuantityRows = pros.QuantityRows,
-                    TotalPages = pros.TotalPages
-                }
-            };
+        //public IDemoQuery DemoQuery { get; set; }
+        //public IDemoTransaction DemoTransaction { get; set; }
+        public ITableQuery TableQuery { get; set; }
 
-            var response = new Response<CollectionDataResponse<PersonResponse>>()
-            {
-                Value = collectionDataResponse
-            };
-            return response;
-        }
-
-        public Response<List<ProyectoResponse>> ListProyectos()
-        {
-            var result = DemoQuery.ListProyectos().ToList();
-
-            var response = new Response<List<ProyectoResponse>>()
-            {
-                Value = result
-            };
-            return response;
-        }
-
-        public Response<ProyectoResponse> GetProyectoById(ProyectoRequest proyectoRequest)
-        {
-            var result = DemoQuery.GetProyectoById(proyectoRequest);
-            return result;
-        }
-
-        
-
-        public Response<PersonResponse> GetPersonById(PersonBaseRequest personBaseRequest)
-        {
-            var result = DemoQuery.GetPersonById(personBaseRequest);
-            return result;
-        }
-
-        public Response<int> RegPerson(PersonRequest personRequest, BaseRecordRequest baseRecordRequest)
-        {
-            var result = new Response<int>(1);
-            using (var transaction = new TransactionScope())
-            {
-                try
-                {
-                    var personEntity =
-                        map.Mapper.Map<PersonRequest, PersonEntity>(personRequest);
-                    DemoTransaction.RegPerson(personEntity, baseRecordRequest);
-                    transaction.Complete();
-                }
-                catch (Exception e)
-                {
-                    TraceLogger.RegisterExceptionDemand(JsonConvert.SerializeObject(e));
-                    result = new Response<int>(0);
-                }
-            }
-
-            return result;
-        }
-
-        public Response<int> UpdPersonState(PersonBaseRequest personBaseRequest, BaseRecordRequest baseRecordRequest)
-        {
-            var result = new Response<int>(1);
-            using (var transaction = new TransactionScope())
-            {
-                try
-                {
-                    DemoTransaction.UpdPersonState(personBaseRequest, baseRecordRequest);
-                    transaction.Complete();
-                }
-                catch (Exception e)
-                {
-                    TraceLogger.RegisterExceptionDemand(JsonConvert.SerializeObject(e));
-                    result = new Response<int>(0);
-                }
-            }
-
-            return result;
-        }
 
 
         /// <summary>
@@ -128,33 +35,33 @@ namespace BaseArchitecture.Application.Service.Table
         ///     The method GetMasterById get list MasterTable and filter by IdMasterTable
         /// </remarks>
         /// <returns>Return entity MasterTableResponse</returns>
-        public MasterTableResponse GetMasterById(MasterTableRequest masterTableRequest)
+        public MasterTableEntity GetMasterById(MasterTableEntity masterTableRequest)
         {
-            var result = GetAllMaster(masterTableRequest);
+            var result = GetAllMaster();
             return result.FirstOrDefault(x => x.IdMasterTable == masterTableRequest.IdMasterTable);
         }
 
-        public Response<IEnumerable<MasterTableResponse>> ListMasterTable(MasterTableRequest masterTableRequest)
+        public Response<IEnumerable<MasterTableEntity>> ListMasterTable(MasterTableEntity masterTableRequest)
         {
-            var result = GetAllMaster(masterTableRequest);
+            var result = GetAllMaster();
             var masterTableResponses =
                 result.Where(x => x.IdMasterTableParent == masterTableRequest.IdMasterTableParent).ToList();
 
-            return new Response<IEnumerable<MasterTableResponse>> { Value = masterTableResponses };
+            return new Response<IEnumerable<MasterTableEntity>> { Value = masterTableResponses };
         }
 
-        public Response<IEnumerable<MasterTableResponse>> ListMasterTableByValue(MasterTableRequest masterTableRequest)
+        public Response<IEnumerable<MasterTableEntity>> ListMasterTableByValue(MasterTableEntity masterTableRequest)
         {
-            var result = GetAllMaster(masterTableRequest);
+            var result = GetAllMaster();
             var masterTableResponses = result.Where(x => x.IdMasterTableParent == masterTableRequest.IdMasterTableParent
                                                          && x.Value == masterTableRequest.Value).ToList();
 
-            return new Response<IEnumerable<MasterTableResponse>> { Value = masterTableResponses };
+            return new Response<IEnumerable<MasterTableEntity>> { Value = masterTableResponses };
         }
 
-        private IEnumerable<MasterTableResponse> GetAllMaster(MasterTableRequest masterTableRequest)
+        private IEnumerable<MasterTableEntity> GetAllMaster()
         {
-            return DemoQuery.ListMasterTable(masterTableRequest);
+            return TableQuery.ListMasterTable();
         }
     }
 }
