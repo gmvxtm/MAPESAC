cb1430661a2c379de231e165ffc7b08c72bd5f22
Percy Angel Pardo Zapata <ppardoz09@gmail.com>
2021-09-14 13:13:33 -0500

    Entidad para mapear los Insumos por producto

---
+++ APLICACION/BaseArchitecture/BaseArchitecture.Repository.Entity/Tables/SupplyEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseArchitecture.Repository.Entity.Tables
+{
+    public class SupplyEntity
+    {
+        
+    }
+}
