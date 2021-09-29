70bd1db990ac38e9938f7308ebd9cb823510777f
Percy Angel Pardo Zapata <ppardoz09@gmail.com>
2021-09-28 20:05:37 -0500

    BD

---
+++ BD/Insert SubProcessProduct and SubOrderFlow - Data de Prueba.sql
@@ -0,0 +1,115 @@
+SELECT NEWID();
+
+SELECT	*
+FROM	MasterTable
+
+SELECT	*
+FROM	ProductSubProcess
+ORDER BY IdProduct, OrderSubProcess ASC
+
+INSERT INTO ProductSubProcess	
+	(IdProducSubProcess, IdProduct, LocationOrder, [Description], OrderSubProcess)
+VALUES
+	(NEWID(), 'D361E525-DEB5-4666-9DBD-1ACB12A543AF', '00202', 'Corte de tela para elaboración de prenda.', 1)
+	,(NEWID(), 'D361E525-DEB5-4666-9DBD-1ACB12A543AF', '00203', 'Armado de la prenda.', 2)
+	,(NEWID(), 'D361E525-DEB5-4666-9DBD-1ACB12A543AF', '00204', 'Lavado de prendas de color blanco.', 3)
+	,(NEWID(), 'D361E525-DEB5-4666-9DBD-1ACB12A543AF', '00204', 'Lavado de prendas de color negro.', 4)
+	,(NEWID(), 'D361E525-DEB5-4666-9DBD-1ACB12A543AF', '00204', 'Lavado de prendas de color azul.', 5)
+	,(NEWID(), 'D361E525-DEB5-4666-9DBD-1ACB12A543AF', '00205', 'Acabados de la prenda.', 6)
+
+	,(NEWID(), '76D5F7F6-2BD4-4718-9BEF-2F21A381CB9F', '00202', 'Corte de tela para elaboración de prenda.', 1)
+	,(NEWID(), '76D5F7F6-2BD4-4718-9BEF-2F21A381CB9F', '00203', 'Armado de la prenda.', 2)
+	,(NEWID(), '76D5F7F6-2BD4-4718-9BEF-2F21A381CB9F', '00204', 'Lavado de prendas de color blanco.', 3)
+	,(NEWID(), '76D5F7F6-2BD4-4718-9BEF-2F21A381CB9F', '00204', 'Lavado de prendas de color negro.', 4)
+	,(NEWID(), '76D5F7F6-2BD4-4718-9BEF-2F21A381CB9F', '00204', 'Lavado de prendas de color azul.', 5)
+	,(NEWID(), '76D5F7F6-2BD4-4718-9BEF-2F21A381CB9F', '00205', 'Acabados de la prenda.', 6)
+
+	,(NEWID(), 'E89A8A6E-8B96-4DA7-B85A-570BD7D9850F', '00202', 'Corte de tela para elaboración de prenda.', 1)
+	,(NEWID(), 'E89A8A6E-8B96-4DA7-B85A-570BD7D9850F', '00203', 'Armado de la prenda.', 2)
+	,(NEWID(), 'E89A8A6E-8B96-4DA7-B85A-570BD7D9850F', '00204', 'Lavado de prendas de color blanco.', 3)
+	,(NEWID(), 'E89A8A6E-8B96-4DA7-B85A-570BD7D9850F', '00204', 'Lavado de prendas de color negro.', 4)
+	,(NEWID(), 'E89A8A6E-8B96-4DA7-B85A-570BD7D9850F', '00204', 'Lavado de prendas de color azul.', 5)
+	,(NEWID(), 'E89A8A6E-8B96-4DA7-B85A-570BD7D9850F', '00205', 'Acabados de la prenda.', 6)
+
+	,(NEWID(), '82451157-5164-4DFB-8652-581A66595A9B', '00202', 'Corte de tela para elaboración de prenda.', 1)
+	,(NEWID(), '82451157-5164-4DFB-8652-581A66595A9B', '00203', 'Armado de la prenda.', 2)
+	,(NEWID(), '82451157-5164-4DFB-8652-581A66595A9B', '00204', 'Lavado de prendas de color blanco.', 3)
+	,(NEWID(), '82451157-5164-4DFB-8652-581A66595A9B', '00204', 'Lavado de prendas de color negro.', 4)
+	,(NEWID(), '82451157-5164-4DFB-8652-581A66595A9B', '00204', 'Lavado de prendas de color azul.', 5)
+	,(NEWID(), '82451157-5164-4DFB-8652-581A66595A9B', '00205', 'Acabados de la prenda.', 6)
+
+	,(NEWID(), 'D18C3091-729F-48E1-940A-81C7F864AFB2', '00202', 'Corte de tela para elaboración de prenda.', 1)
+	,(NEWID(), 'D18C3091-729F-48E1-940A-81C7F864AFB2', '00203', 'Armado de la prenda.', 2)
+	,(NEWID(), 'D18C3091-729F-48E1-940A-81C7F864AFB2', '00204', 'Lavado de prendas de color blanco.', 3)
+	,(NEWID(), 'D18C3091-729F-48E1-940A-81C7F864AFB2', '00204', 'Lavado de prendas de color negro.', 4)
+	,(NEWID(), 'D18C3091-729F-48E1-940A-81C7F864AFB2', '00204', 'Lavado de prendas de color azul.', 5)
+	,(NEWID(), 'D18C3091-729F-48E1-940A-81C7F864AFB2', '00205', 'Acabados de la prenda.', 6)
+
+	,(NEWID(), '4FECB6FF-0508-45C2-A2C2-84C2F51514F6', '00202', 'Corte de tela para elaboración de prenda.', 1)
+	,(NEWID(), '4FECB6FF-0508-45C2-A2C2-84C2F51514F6', '00203', 'Armado de la prenda.', 2)
+	,(NEWID(), '4FECB6FF-0508-45C2-A2C2-84C2F51514F6', '00204', 'Lavado de prendas de color blanco.', 3)
+	,(NEWID(), '4FECB6FF-0508-45C2-A2C2-84C2F51514F6', '00204', 'Lavado de prendas de color negro.', 4)
+	,(NEWID(), '4FECB6FF-0508-45C2-A2C2-84C2F51514F6', '00204', 'Lavado de prendas de color azul.', 5)
+	,(NEWID(), '4FECB6FF-0508-45C2-A2C2-84C2F51514F6', '00205', 'Acabados de la prenda.', 6)
+	
+	,(NEWID(), '3754B17A-7C4F-4A9A-999A-871B700C5272', '00202', 'Corte de tela para elaboración de prenda.', 1)
+	,(NEWID(), '3754B17A-7C4F-4A9A-999A-871B700C5272', '00203', 'Armado de la prenda.', 2)
+	,(NEWID(), '3754B17A-7C4F-4A9A-999A-871B700C5272', '00204', 'Lavado de prendas de color blanco.', 3)
+	,(NEWID(), '3754B17A-7C4F-4A9A-999A-871B700C5272', '00204', 'Lavado de prendas de color negro.', 4)
+	,(NEWID(), '3754B17A-7C4F-4A9A-999A-871B700C5272', '00204', 'Lavado de prendas de color azul.', 5)
+	,(NEWID(), '3754B17A-7C4F-4A9A-999A-871B700C5272', '00205', 'Acabados de la prenda.', 6)
+	
+	,(NEWID(), 'ABD7C103-7799-405A-BD90-9B0CFFD6A82A', '00202', 'Corte de tela para elaboración de prenda.', 1)
+	,(NEWID(), 'ABD7C103-7799-405A-BD90-9B0CFFD6A82A', '00203', 'Armado de la prenda.', 2)
+	,(NEWID(), 'ABD7C103-7799-405A-BD90-9B0CFFD6A82A', '00204', 'Lavado de prendas de color blanco.', 3)
+	,(NEWID(), 'ABD7C103-7799-405A-BD90-9B0CFFD6A82A', '00204', 'Lavado de prendas de color negro.', 4)
+	,(NEWID(), 'ABD7C103-7799-405A-BD90-9B0CFFD6A82A', '00204', 'Lavado de prendas de color azul.', 5)
+	,(NEWID(), 'ABD7C103-7799-405A-BD90-9B0CFFD6A82A', '00205', 'Acabados de la prenda.', 6)
+	
+	,(NEWID(), '86E90A40-278B-41E7-8AFF-B5AB5F36F3AB', '00202', 'Corte de tela para elaboración de prenda.', 1)
+	,(NEWID(), '86E90A40-278B-41E7-8AFF-B5AB5F36F3AB', '00203', 'Armado de la prenda.', 2)
+	,(NEWID(), '86E90A40-278B-41E7-8AFF-B5AB5F36F3AB', '00204', 'Lavado de prendas de color blanco.', 3)
+	,(NEWID(), '86E90A40-278B-41E7-8AFF-B5AB5F36F3AB', '00204', 'Lavado de prendas de color negro.', 4)
+	,(NEWID(), '86E90A40-278B-41E7-8AFF-B5AB5F36F3AB', '00204', 'Lavado de prendas de color azul.', 5)
+	,(NEWID(), '86E90A40-278B-41E7-8AFF-B5AB5F36F3AB', '00205', 'Acabados de la prenda.', 6)
+	
+	,(NEWID(), 'F2F1260D-8B9A-4731-8DF2-BFA4F697AAF6', '00202', 'Corte de tela para elaboración de prenda.', 1)
+	,(NEWID(), 'F2F1260D-8B9A-4731-8DF2-BFA4F697AAF6', '00203', 'Armado de la prenda.', 2)
+	,(NEWID(), 'F2F1260D-8B9A-4731-8DF2-BFA4F697AAF6', '00204', 'Lavado de prendas de color blanco.', 3)
+	,(NEWID(), 'F2F1260D-8B9A-4731-8DF2-BFA4F697AAF6', '00204', 'Lavado de prendas de color negro.', 4)
+	,(NEWID(), 'F2F1260D-8B9A-4731-8DF2-BFA4F697AAF6', '00204', 'Lavado de prendas de color azul.', 5)
+	,(NEWID(), 'F2F1260D-8B9A-4731-8DF2-BFA4F697AAF6', '00205', 'Acabados de la prenda.', 6)
+	
+	,(NEWID(), 'E8380175-59BE-4CDF-AF06-D39F7CAF4C43', '00202', 'Corte de tela para elaboración de prenda.', 1)
+	,(NEWID(), 'E8380175-59BE-4CDF-AF06-D39F7CAF4C43', '00203', 'Armado de la prenda.', 2)
+	,(NEWID(), 'E8380175-59BE-4CDF-AF06-D39F7CAF4C43', '00204', 'Lavado de prendas de color blanco.', 3)
+	,(NEWID(), 'E8380175-59BE-4CDF-AF06-D39F7CAF4C43', '00204', 'Lavado de prendas de color negro.', 4)
+	,(NEWID(), 'E8380175-59BE-4CDF-AF06-D39F7CAF4C43', '00204', 'Lavado de prendas de color azul.', 5)
+	,(NEWID(), 'E8380175-59BE-4CDF-AF06-D39F7CAF4C43', '00205', 'Acabados de la prenda.', 6)
+
+INSERT INTO	SubOrderFlow
+	(IdSubOrderFlow, IdProductSubProcess, IdOrderFlow, StatusSubOrderMT)
+SELECT	NEWID()
+		,PSP.IdProductSubProcess
+		,[OF].IdOrderFlow
+		,'00101'
+FROM	ProductSubProcess	PSP
+INNER JOIN	OrderDetail	OD
+	ON	OD.IdProduct	=	PSP.IdProduct
+INNER JOIN	Product	P
+	ON	OD.IdProduct	=	P.IdProduct
+INNER JOIN	[Order]	O
+	ON	OD.IdOrder	=	O.IdOrder
+INNER JOIN	OrderFlow	[OF]
+	ON	O.IdOrder	=	[OF].IdOrder
+	AND	[OF].LocationOrder	=	PSP.LocationOrder
+
+SELECT	*
+FROM	[OrderDetail]
+WHERE	IdOrder = '15386A0D-4867-44D3-BAF3-91636514D2CB'
+
+SELECT	*
+FROM	[OrderFlow]
+ORDER BY	IdOrder, NroOrder ASC
+
+MasterTable
