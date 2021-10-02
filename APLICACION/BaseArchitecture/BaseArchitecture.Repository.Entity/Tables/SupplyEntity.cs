fb23dd99d99ec7453dffbbc9d67e7812c687f123
Gino Angelo Maguina Vera <gino.maguina@mdp.com.pe>
2021-10-02 17:00:02 -0500

    a

--- APLICACION/BaseArchitecture/BaseArchitecture.Repository.Entity/Tables/SupplyEntity.cs
+++ APLICACION/BaseArchitecture/BaseArchitecture.Repository.Entity/Tables/SupplyEntity.cs
@@ -8,7 +8,7 @@ namespace BaseArchitecture.Repository.Entity.Tables
 {
     public class SupplyEntity
     {
-        public string IdSupply { get; set; }
+        public Guid IdSupply { get; set; }
         public string CodeSupply { get; set; }
         public string Name { get; set; }
         public double Stock { get; set; }
@@ -18,3 +18,5 @@ namespace BaseArchitecture.Repository.Entity.Tables
         public string DateUpdate { get; set; }
     }
 }
+
+
